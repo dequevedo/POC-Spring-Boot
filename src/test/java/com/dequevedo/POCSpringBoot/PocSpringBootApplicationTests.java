5c520c01054ebea1fdd8b5a075a63cc61976a40f
Daniel Quevedo <danielelvisquevedo@gmail.com>
2022-06-25 16:26:34 -0300

    feat(POC): Second commit

---
+++ src/test/java/com/dequevedo/POCSpringBoot/PocSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dequevedo.POCSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
