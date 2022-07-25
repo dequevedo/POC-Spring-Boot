f51380ed026321b1370d9de680d6f9bf19c1b109
Daniel Quevedo <daniel.quevedo@sensedia.com>
2022-07-25 16:51:34 -0300

    feat(POC): Nada funciona u.u

---
+++ src/test/java/com/community/farm/service/SensorDataServiceTest.java
@@ -0,0 +1,12 @@
+package com.community.farm.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SensorDataServiceTest {
+
+    @Test
+    void postSensorData() {
+    }
+}
